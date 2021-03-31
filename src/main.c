73b2e2c470baca26180883d4acf87035a5c95d9c
d0p1 <contact@d0p1.eu>
2021-03-31 13:18:46 +0200

    feat: force init to be run on pid 1

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int
 main(void)
 {
+	if (getpid() != 1)
+	{
+		fprintf(stderr, "init must be run at pid 1\n");
+		return (EXIT_FAILURE);
+	}
+
+	chdir("/");
+	while (1)
+	{
+	}
+
+	/* unreachable */
 	return (EXIT_SUCCESS);
 }
